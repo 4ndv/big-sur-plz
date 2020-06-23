25972151a00c387296a1eccb2dd35ad096e0c909
Andrey Viktorov <andreyviktorov@outlook.com>
2020-06-24 04:22:19 +0700

    First release

---
+++ README.md
@@ -0,0 +1,47 @@
+# Big Sur, Please
+
+[Buy me a ☕️ if you like my projects :)](https://lynx.pink/buymeacoffee)
+
+----------
+
+## What's this?
+
+It's a simple script, that automates some of the actions, required to setup Big Sur on unsupported macs.
+
+Follow this topic on MacRumors for more info: [https://forums.macrumors.com/threads/macos-11-big-sur-on-unsupported-macs-thread.2242172/](https://forums.macrumors.com/threads/macos-11-big-sur-on-unsupported-macs-thread.2242172/)
+
+Please report any issues in the github issues section.
+
+## How to run?
+
+Clone repository:
+
+```bash
+cd ~
+git clone https://github.com/4ndv/big-sur-plz.git
+```
+
+Navigate to `big-sur-plz` folder:
+
+```bash
+cd ~/big-sur-plz
+```
+
+Run with default installer path:
+
+```bash
+/usr/bin/ruby big_sur_plz.rb
+```
+
+Run with custom installer path:
+
+```bash
+/usr/bin/ruby big_sur_plz.rb "/Users/user/Downloads/Install macOS Beta.app"
+```
+
+And follow the instructions
+
+## Huge thanks
+
+- [ASentientBot](https://forums.macrumors.com/members/asentientbot.1135186/) for the installer patcher
+- MacRumors community in a nutshell
---
+++ big_sur_plz.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+def yes
+  STDIN.gets.chomp == 'yes'
+end
+
+#
+# Check installer
+#
+
+installer_path = ARGV[0] || '/Applications/Install macOS Beta.app'
+
+assistant_path = "#{installer_path}/Contents/MacOS/InstallAssistant"
+
+unless File.exist?(assistant_path)
+  puts '[ERROR] Couldn\'t find installer in the default location'
+  puts '[ERROR] Specify correct path to Big Sur installer'
+  exit 1
+end
+
+puts "Found installer at #{installer_path}"
+puts "Found InstallAssistant at #{assistant_path}"
+
+#
+# SIP
+#
+puts 'Checking SIP status'
+
+sip_status_output = `csrutil status`
+
+unless sip_status_output.include? 'disabled'
+  puts '[ERROR] SIP is enabled. Reboot to recovery mode (Cmd+R and power on) and enter "csrutil disable" in terminal'
+  exit 1
+end
+
+#
+# FileVault
+#
+
+puts 'Checking FileVault status'
+
+filevault_status_output = `fdesetup status`
+
+if filevault_status_output.include? 'is On.'
+  puts '[WARN] FileVault is Enabled and may cause some issues. I HAVEN\'T CHECKED how it affects installation.'
+  puts '[WARN] Please, if you decide to proceed with FileVault enabled'
+  puts 'after installation try to remount root as rw and submit results'
+  puts '[WARN] If you want to try proceeding with FileVault enabled, enter "yes":'
+
+  exit 1 unless yes
+end
+
+#
+# Library validation
+#
+
+puts 'Disabling library validation'
+
+#
+# Add boot vars
+#
+
+`sudo defaults write /Library/Preferences/com.apple.security.libraryvalidation.plist DisableLibraryValidation -bool true`
+
+puts 'Checking current boot args'
+
+current_boot_args = `sudo nvram boot-args`
+
+unless current_boot_args.include? 'Error getting variable'
+  puts '[WARN] There is already boot args set:'
+  puts current_boot_args
+  puts '[WARN] Type "yes" to overwrite it:'
+
+  if yes
+    `sudo nvram boot-args="-no_compat_check"`
+  else
+    puts 'Continuing without setting boot args'
+  end
+end
+
+puts 'Downloading patcher'
+
+if File.exist?('Hax.dylib') && File.exist?('InstallHax.m') && File.exist?('patcher.zip')
+  puts 'Patcher already exists, skipping download'
+else
+  `curl https://forums.macrumors.com/attachments/really-simple-installer-hack-zip.926156/ -o patcher.zip`
+
+  puts 'Extracting patcher'
+
+  `unzip patcher.zip`
+end
+
+puts 'Running installer'
+
+installer = fork do
+  exec "/bin/bash -c 'DYLD_INSERT_LIBRARIES=#{File.expand_path('Hax.dylib')} #{assistant_path.gsub(/ /, '\ ')}'"
+end
+
+puts 'Detaching installer'
+
+Process.detach(installer)
